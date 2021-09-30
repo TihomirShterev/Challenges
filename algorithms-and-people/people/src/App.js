e602e75c549e0b637aa59185a604729e9f4c2d4d
Tihomir Shterev <tihomir.shterev.1@gmail.com>
2021-09-30 09:33:54 +0300

    Initial setup
    Installed dependencies, updated favicon, title, added readme and .gitignore

---
+++ algorithms-and-people/people/src/App.js
@@ -0,0 +1,12 @@
+import Table from './components/table';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
